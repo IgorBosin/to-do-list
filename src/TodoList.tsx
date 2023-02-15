5b28f83a6087d303d83d489d85b2bc473f369cf5
IgorBosin <ibosin@yandex.ru>
2023-02-15 20:10:13 +0300

    lesson #1 (map)

---
+++ src/TodoList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+type TodoListType = {
+    tasks: TasksType[]
+}
+
+type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const TodoList = (props: TodoListType) => {
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(el => {
+                    return (
+                        <li key={el.id}><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                    )
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
